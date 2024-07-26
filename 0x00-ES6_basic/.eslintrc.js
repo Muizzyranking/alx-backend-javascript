0e65cc8702d854aab4a3d809ce0c050bc7403b6e
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-07-26 18:18:15 +0100

    config files
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -13,11 +13,17 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["jest"],
+  plugins: ["jest", "import"],
   rules: {
     "no-console": "off",
     "no-shadow": "off",
     "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+    "import/extensions": ["error", "always", {
+      "js": "always",
+      "jsx": "never",
+      "ts": "never",
+      "tsx": "never"
+    }]
   },
   overrides: [
     {
