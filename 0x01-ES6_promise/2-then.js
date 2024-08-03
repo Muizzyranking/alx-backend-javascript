f58c7cf6687c355279a4b873e50f1253d1d0f632
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-08-03 16:00:41 +0100

    catch me if you can
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+export default function handleResponseFromAPI(promise) {
+  const ret = promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+  return ret;
+}
