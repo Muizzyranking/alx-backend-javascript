436ce1864ab2da797613ae5688357df50a0feeef
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-08-06 13:13:44 +0100

    (fix): Handle multiple promises
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,10 +5,10 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
 
-  const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
+  const results = Promise.allSettled([signUpPromise, uploadPhotoPromise]);
 
   return results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
   }));
 }
